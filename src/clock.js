a3b144ff06efc00ed59865f4ee261a7111c9cd23
f0x <f0x@cthu.lu>
2025-01-27 15:46:28 +0100

    update network indicator, hadn styling

--- src/clock.js
+++ src/clock.js
@@ -16,54 +16,50 @@ function pad(fragment) {
 	return fragment.toString().padStart(2, "0");
 }
 
-function AnalogClock({time, name}) {
-	let hour_x = Math.cos((time.hours-3 + (time.mins/60))/6*Math.PI) * (35);
-	let hour_y = Math.sin((time.hours-3 + (time.mins/60))/6*Math.PI) * (35);
+function AnalogClock({ time, name }) {
+	let hour_x = Math.cos((time.hours - 3 + (time.mins / 60)) / 6 * Math.PI) * (35);
+	let hour_y = Math.sin((time.hours - 3 + (time.mins / 60)) / 6 * Math.PI) * (35);
 
-	let hour_x_contra = Math.cos((time.hours-3 + (time.mins/60))/6*Math.PI - Math.PI) * 10;
-	let hour_y_contra = Math.sin((time.hours-3 + (time.mins/60))/6*Math.PI - Math.PI) * 10;
+	let hour_x_contra = Math.cos((time.hours - 3 + (time.mins / 60)) / 6 * Math.PI - Math.PI) * 10;
+	let hour_y_contra = Math.sin((time.hours - 3 + (time.mins / 60)) / 6 * Math.PI - Math.PI) * 10;
 
-	let min_x = Math.cos((time.mins-15 + (time.secs/60))/30*Math.PI) * 70;
-	let min_y = Math.sin((time.mins-15 + (time.secs/60))/30*Math.PI) * 70;
+	let min_x = Math.cos((time.mins - 15 + (time.secs / 60)) / 30 * Math.PI) * 70;
+	let min_y = Math.sin((time.mins - 15 + (time.secs / 60)) / 30 * Math.PI) * 70;
 
-	let min_x_contra = Math.cos((time.mins-15 + (time.secs/60))/30*Math.PI - Math.PI) * 15;
-	let min_y_contra = Math.sin((time.mins-15 + (time.secs/60))/30*Math.PI - Math.PI) * 15;
+	let min_x_contra = Math.cos((time.mins - 15 + (time.secs / 60)) / 30 * Math.PI - Math.PI) * 15;
+	let min_y_contra = Math.sin((time.mins - 15 + (time.secs / 60)) / 30 * Math.PI - Math.PI) * 15;
 
-	let sec_x = Math.cos((time.secs-15)/30*Math.PI) * 75;
-	let sec_y = Math.sin((time.secs-15)/30*Math.PI) * 75;
+	let sec_x = Math.cos((time.secs - 15) / 30 * Math.PI) * 75;
+	let sec_y = Math.sin((time.secs - 15) / 30 * Math.PI) * 75;
 
-	let sec_x_contra = Math.cos((time.secs-15)/30*Math.PI - Math.PI) * 10;
-	let sec_y_contra = Math.sin((time.secs-15)/30*Math.PI - Math.PI) * 10;
+	let sec_x_contra = Math.cos((time.secs - 15) / 30 * Math.PI - Math.PI) * 10;
+	let sec_y_contra = Math.sin((time.secs - 15) / 30 * Math.PI - Math.PI) * 10;
 
 	return (
 		<svg className={"analog " + name} viewBox="-100 -100 200 200">
 			<line x1={0} y1={0} x2={hour_x} y2={hour_y}
-				stroke="#1e1e1e" strokeWidth="3.8" />
+				stroke="#1e1e1e" strokeWidth="3.8" strokeLinecap="round" />
 			<line x1={0} y1={0} x2={hour_x_contra} y2={hour_y_contra}
 				stroke="#1e1e1e" strokeWidth="3.8" />
-			<circle cx={hour_x} cy={hour_y}
-				r={1.35} stroke="#1e1e1e" fill="#1e1e1e" />
 
 			<line x1={0} y1={0} x2={min_x} y2={min_y}
-				stroke="#1e1e1e" strokeWidth="3.5" />
+				stroke="#1e1e1e" strokeWidth="3.5" strokeLinecap="round" />
 			<line x1={0} y1={0} x2={min_x_contra} y2={min_y_contra}
 				stroke="#1e1e1e" strokeWidth="3.5" />
-			<circle cx={min_x} cy={min_y}
-				r={1.35} stroke="#1e1e1e" fill="#1e1e1e" />
+
+			<circle cx={0} cy={0} r={2.6} stroke="#1e1e1e" fill="#1e1e1e" />
 
 			<line x1={0} y1={0} x2={sec_x} y2={sec_y}
-				stroke="red" strokeWidth="1.4" />
+				stroke="red" strokeWidth="1.4" strokeLinecap="round" />
 			<line x1={0} y1={0} x2={sec_x_contra} y2={sec_y_contra}
 				stroke="red" strokeWidth="1.4" />
-			<circle cx={sec_x} cy={sec_y}
-				r={0.3} stroke="red" fill="red" />
 
-			<circle cx={0} cy={0} r={2.1} stroke="#1e1e1e" fill="#1e1e1e" />
+			<circle cx={0} cy={0} r={1.6} stroke="red" fill="red" />
 		</svg>
 	);
 }
 
-module.exports = function Clock({legend, date, offset}) {
+module.exports = function Clock({ legend, date, offset }) {
 	if (offset == "local") {
 		offset = date.getTimezoneOffset() / 60 * - 1;
 	}
@@ -73,7 +69,7 @@ module.exports = function Clock({legend, date, offset}) {
 	return (
 		<div className="clock">
 			<div className="legend">{legend}</div>
-			<AnalogClock time={time} name={legend}/>
+			<AnalogClock time={time} name={legend} />
 			<div className="digital">{pad(time.hours)}<span className="gray">:{pad(time.mins)}</span></div>
 		</div>
 	);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 "use strict";
 
 const React = require("react");
-const {createRoot} = require("react-dom/client");
+const { createRoot } = require("react-dom/client");
+const sAgo = require("s-ago");
 
 const useSpecialEvents = require("./special-events");
 const Clock = require("./clock");
@@ -9,16 +10,17 @@ const Clock = require("./clock");
 require("./style.css");
 
 function App() {
-	const [config, setConfig] = React.useState({clocks: [], networked: false});
+	const [config, setConfig] = React.useState({ clocks: [], networked: false });
 	const [date, setDate] = React.useState(new Date());
 	const [hasNetwork, setNetwork] = React.useState(true);
+	const [lastPing, setLastPing] = React.useState(null);
 
 	const event = useSpecialEvents(date);
 
 	React.useEffect(() => {
 		const id = setInterval(() => {
 			setDate(new Date());
-		}, 200);
+		}, 100);
 
 		return () => clearInterval(id);
 	}, []);
@@ -42,20 +44,17 @@ function App() {
 				return;
 			}
 
-			console.log("ping");
-			fetch("https://google.nl", {mode: 'no-cors'})
+			fetch("https://google.nl", { mode: 'no-cors' })
 				.then((response) => {
 					if (response.ok || response.type == "opaque") { // if blocked by CORS we're online
 						setNetwork(true);
+						setLastPing(new Date());
 					} else {
-						console.log(response.ok, response.type);
 						setNetwork(false);
 					}
-					console.log("pong");
 					setTimeout(ping, 30000);
 				})
-				.catch((e) => {
-					console.error(e);
+				.catch(() => {
 					setNetwork(false);
 					setTimeout(ping, 5000);
 				});
@@ -68,31 +67,42 @@ function App() {
 
 	return (
 		<React.Fragment>
-			{}
 			{event.element}
 			<div className={"clock-container " + event.className}>
 				{config.clocks.map((clock, id) => {
 					return (
 						<Clock
-							key = {id}
-							legend = {clock.legend}
-							offset = {clock.offset}
-				
-							date = {date}
+							key={id}
+							legend={clock.legend}
+							offset={clock.offset}
+							date={date}
 						/>
 					);
 				})}
 			</div>
-      {!hasNetwork && (
-				<div className="noping">
-					Geen internetverbinding!<br/>
-					Echte tijd kan afwijken van weergeven tijd
-				</div>
-			)}
+			{
+				config.networked && (
+					<div className="network">
+						{hasNetwork
+							? <div>
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M54.2 202.9C123.2 136.7 216.8 96 320 96s196.8 40.7 265.8 106.9c12.8 12.2 33 11.8 45.2-.9s11.8-33-.9-45.2C549.7 79.5 440.4 32 320 32S90.3 79.5 9.8 156.7C-2.9 169-3.3 189.2 8.9 202s32.5 13.2 45.2 .9zM320 256c56.8 0 108.6 21.1 148.2 56c13.3 11.7 33.5 10.4 45.2-2.8s10.4-33.5-2.8-45.2C459.8 219.2 393 192 320 192s-139.8 27.2-190.5 72c-13.3 11.7-14.5 31.9-2.8 45.2s31.9 14.5 45.2 2.8c39.5-34.9 91.3-56 148.2-56zm64 160a64 64 0 1 0 -128 0 64 64 0 1 0 128 0z" /></svg>
+								Connected
+							</div>
+							: <div className="error">
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+									<path d="M54.2 202.9C123.2 136.7 216.8 96 320 96s196.8 40.7 265.8 106.9c12.8 12.2 33 11.8 45.2-.9s11.8-33-.9-45.2C549.7 79.5 440.4 32 320 32S90.3 79.5 9.8 156.7C-2.9 169-3.3 189.2 8.9 202s32.5 13.2 45.2 .9zM320 256c56.8 0 108.6 21.1 148.2 56c13.3 11.7 33.5 10.4 45.2-2.8s10.4-33.5-2.8-45.2C459.8 219.2 393 192 320 192s-139.8 27.2-190.5 72c-13.3 11.7-14.5 31.9-2.8 45.2s31.9 14.5 45.2 2.8c39.5-34.9 91.3-56 148.2-56zm64 160a64 64 0 1 0 -128 0 64 64 0 1 0 128 0z" />
+									<line id="red" x1="160" y1="120" x2="480" y2="392" strokeLinecap="round" strokeWidth="50" />
+								</svg>
+								{lastPing ? <>Last connected: {sAgo(lastPing)}</> : "Not connected"}
+							</div>
+						}
+					</div>
+				)
+			}
 			<h1>the wired</h1>
 		</React.Fragment>
 	);
 }
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App/>);
+root.render(<App />);
